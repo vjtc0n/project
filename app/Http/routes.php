306e1c54f954ac4c3e94c3104fd7691ed11b5201
khiemdoancrazy <doankhiem.crazy@gmail.com>
2015-11-04 14:03:49 +0700

    UpdateRoute
    update lại  route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,4 +19,19 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
 
