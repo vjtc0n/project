59645102229fe32b021e98058c2e81f498fab811
vjtc0n <vjtc0n12345@gmail.com>
2015-11-22 16:23:40 +0700

    Fixed Menu
    Đã sửa xong

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -54,6 +54,7 @@
           <ul class="nav navbar-nav navbar-right">
             @if (Auth::guest())
             <li><a href="{{ url('/login') }}">Login</a></li>
+            
             <script>
               $(function() {
               $('#exampleModal').modal('show');
@@ -63,15 +64,11 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                 {{ Auth::user()->name }}
-                <span class="caret"></span>
-              </a>
+                
+              </a></li>
+              <li><a href="{{ url('/logout') }}">Logout</a></li>
 
-
-              <ul class="dropdown-menu" role="menu">
-                <li><a role="menuitem" href="{{ url('/logout') }}">Logout</a></li>
-
-              </ul>
-            </li>
+            
             @endif
           </ul>
         </div>
