81d66c29df398d40b7c867fbbac6b5b24380e79d
khiemdoancrazy <doankhiem.crazy@gmail.com>
2015-11-25 17:58:42 +0700

    thêm seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,10 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(PermissionUsersTableSeeder::class);
+        $this->call(TruongsTableSeeder::class);
+        $this->call(NganhsTableSeeder::class);
+        $this->call(ThiSinhsTableSeeder::class);
+        $this->call(DiemsTableSeeder::class);
 
         Model::reguard();
     }
@@ -39,7 +43,12 @@ class UsersTableSeeder extends Seeder {
     public function run() {
         $now = date('Y-m-d H:i:s');
         DB::table('users')->insert([
-            ['username' => 'admin', 'password' => Hash::make('admin'), 'name' => 'Admin', 'created_at' => $now, 'updated_at' => $now]
+            ['username' => 'admin', 'password' => Hash::make('admin'), 'name' => 'Admin', 'created_at' => $now, 'updated_at' => $now],
+            ['username' => 'staffmanager', 'password' => Hash::make('123'), 'name' => 'staffmanager', 'created_at' => $now, 'updated_at' => $now],
+            ['username' => 'cstaff', 'password' => Hash::make('123'), 'name' => 'cstaff', 'created_at' => $now, 'updated_at' => $now],
+            ['username' => 'ustaff', 'password' => Hash::make('123'), 'name' => 'ustaff', 'created_at' => $now, 'updated_at' => $now],
+            ['username' => 'Nguyen Van A', 'password' => Hash::make('123'), 'name' => 'Nguyễn Văn A', 'created_at' => $now, 'updated_at' => $now],
+            ['username' => 'Nguyen Thi B', 'password' => Hash::make('123'), 'name' => 'Nguyễn Thị B', 'created_at' => $now, 'updated_at' => $now]
         ]);
     }
 }
@@ -47,8 +56,53 @@ class UsersTableSeeder extends Seeder {
 class PermissionUsersTableSeeder extends Seeder {
     public function run() {
         $now = date('Y-m-d H:i:s');
-        DB::table('permission_user')->insert(
-            ['permission_id' => 1, 'user_id' => 1, 'created_at' => $now, 'updated_at' => $now]
-        );
+        DB::table('permission_user')->insert([
+            ['permission_id' => 1, 'user_id' => 1, 'created_at' => $now, 'updated_at' => $now],
+            ['permission_id' => 2, 'user_id' => 2, 'created_at' => $now, 'updated_at' => $now],
+            ['permission_id' => 3, 'user_id' => 3, 'created_at' => $now, 'updated_at' => $now],
+            ['permission_id' => 4, 'user_id' => 4, 'created_at' => $now, 'updated_at' => $now],
+            ['permission_id' => 5, 'user_id' => 5, 'created_at' => $now, 'updated_at' => $now],
+            ['permission_id' => 5, 'user_id' => 6, 'created_at' => $now, 'updated_at' => $now]
+        ]);
+    }
+}
+
+class TruongsTableSeeder extends Seeder {
+    public function run() {
+        $now = date('Y-m-d H:i:s');
+        DB::table('truongs')->insert([
+            ['matr' => 'BKA', 'tentr' => 'Đại học Bách khoa Hà Nội', 'created_at' => $now, 'updated_at' => $now],
+            ['matr' => 'NEU', 'tentr' => 'Đại học Kinh tế Quốc dân', 'created_at' => $now, 'updated_at' => $now],
+        ]);
+    }
+}
+
+class NganhsTableSeeder extends Seeder {
+    public function run() {
+        $now = date('Y-m-d H:i:s');
+        DB::table('nganhs')->insert([
+            ['manganh' => 'BKACNTT', 'tennganh' => 'Công nghệ thông tin', 'diemchuan' => 26, 'truong_id' => 1, 'created_at' => $now, 'updated_at' => $now],
+            ['manganh' => 'NEUTUD', 'tennganh' => 'Toán ứng dụng', 'diemchuan' => 24, 'truong_id' => 2, 'created_at' => $now, 'updated_at' => $now],
+        ]);
+    }
+}
+
+class ThiSinhsTableSeeder extends Seeder {
+    public function run() {
+        $now = date('Y-m-d H:i:s');
+        DB::table('thi_sinhs')->insert([
+            ['ten' => 'Nguyễn Văn A', 'gioitinh' => 'Nam', 'namsinh' => 1997, 'quequan' => 'Hà Nội', 'khuvuc' => '1', 'user_id' => 5, 'created_at' => $now, 'updated_at' => $now],
+            ['ten' => 'Nguyễn Thị B', 'gioitinh' => 'Nữ', 'namsinh' => 1997, 'quequan' => 'Hà Nội', 'khuvuc' => '2', 'user_id' => 6, 'created_at' => $now, 'updated_at' => $now]
+        ]);
+    }
+}
+
+class DiemsTableSeeder extends Seeder {
+    public function run() {
+        $now = date('Y-m-d H:i:s');
+        DB::table('diems')->insert([
+            ['sbd' => 'BKA1234', 'mon1' => 9, 'mon2' => 8, 'mon3' => 7, 'khoi' => 'A', 'thisinh_id' => 1, 'created_at' => $now, 'updated_at' => $now],
+            ['sbd' => 'BKA1235', 'mon1' => 6, 'mon2' => 8, 'mon3' => 10, 'khoi' => 'D1', 'thisinh_id' => 2, 'created_at' => $now, 'updated_at' => $now]
+        ]);
     }
 }
